2e6478d05c2c56c6d248e321dc70170dc48f042b
MihirPrajapati04 <prajapatimihir685@gmail.com>
2025-01-09 15:22:50 +0530

    implemented prediction pipeline and FastAPI app

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -71,3 +71,6 @@ MODEL Evaluation related constants
 MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
 MODEL_BUCKET_NAME = "my-model-mlopsproj2"
 MODEL_PUSHER_S3_KEY = "model-registry"
+
+APP_HOST = "0.0.0.0"
+APP_PORT = 5000
